b087ec8bd01a4ca2568033dcbc446bf113fd8743
Tufael Ahmed <tufaelahmed1110@gmail.com>
2023-02-06 04:02:31 +0600

    socials bar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Socials from "./components/Socials";
 
 const App = () => {
   return (
     <div className="app">
       <div className="noise"></div>
       <Navbar />
+      <Socials />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,5 +1,46 @@
+const data = [
+  {
+    id: 1,
+    title: "Facebook",
+    url: "facebook.com/tufaelahmedbd/",
+  },
+  {
+    id: 2,
+    title: "Github",
+    url: "github.com/tufaelahmedbd",
+  },
+  {
+    id: 3,
+    title: "LinkedIn",
+    url: "linkedin.com/in/tufaelahmedbd/",
+  },
+  {
+    id: 4,
+    title: "Leetcode",
+    url: "leetcode.com/tufaelahmedbd/",
+  },
+  {
+    id: 2,
+    title: "Twitter",
+    url: "twitter.com/tufaelahmedbd",
+  },
+];
 const Socials = () => {
-  return <div></div>;
+  return (
+    <div className="socials flex gap-5 uppercase text-sm fixed left-10 top-[75%] -rotate-90 origin-left text-white/30 p-5 backdrop-blur-xl z-50">
+      {data.map((social) => (
+        <a
+          href={social.url}
+          target="_blank"
+          key={social.id}
+          rel="noreferrer"
+          className=" hover:text-cyan-400 duration-500"
+        >
+          {social.title}
+        </a>
+      ))}
+    </div>
+  );
 };
 
 export default Socials;
